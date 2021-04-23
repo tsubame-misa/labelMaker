033eeb9a8f735130a1c5bef11d2140ec8c391ebc
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-04-23 10:46:40 +0900

    ts2js

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { addOutline } from "ionicons/icons";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>LableMaker</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={addOutline} />
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
