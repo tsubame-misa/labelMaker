24fe164f151c9918c1671ca02445253865cd8c8b
Numakei <ghost.339.kei@gmail.com>
2021-05-14 11:03:17 +0900

    リスト画面の作成

--- src/components/makeList.js
+++ src/components/makeList.js
@@ -1,25 +1,42 @@
 import {
-    IonContent,
-    IonFab,
-    IonFabButton,
-    IonHeader,
-    IonIcon,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-  } from "@ionic/react";
-  import { addOutline } from "ionicons/icons";
-  import "../pages/Home.css";
-  
-  const makeList = () => {
-    return (
-      <IonPage>    
-        <IonContent fullscreen>
-          make List
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default makeList;
-  
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonItemSliding,
+  IonInput,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import { addOutline } from "ionicons/icons";
+import "../pages/Home.css";
+import { useState } from "react";
+
+const useMakeList = () => {
+  const [programName, setProgramName] = useState();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>番組を登録する</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonItem>
+          <IonInput
+            value={programName}
+            placeholder="番組名"
+            onIonChange={(e) => setProgramName(e.detail.value)}
+          ></IonInput>
+        </IonItem>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default useMakeList;
