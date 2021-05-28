faf51d78e7d3c076be58f563e2356fa9d64b0e64
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2021-05-28 10:29:50 +0900

    saaya uploadeed

--- src/components/makeList.js
+++ src/components/makeList.js
@@ -23,6 +23,7 @@ import "../pages/Home.css";
 import { useEffect, useState } from "react";
 
 const MakeList = () => {
+  const [labelName, setLabelName] = useState();
   const [programName, setProgramName] = useState();
   const [data, setData] = useState([]);
 
@@ -30,50 +31,41 @@ const MakeList = () => {
     <IonPage>
       <IonContent>
         <IonHeader>
-          <ion-toolbar>
-            <ion-buttons slot="start">
-              <ion-button>
-                戻る
-          </ion-button>
-            </ion-buttons>
-            <ion-buttons slot="end">
-              <ion-button>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton defaultHref="/" />
+            </IonButtons>
+            <IonButtons slot="end">
+              <IonButton>
                 保存
-          </ion-button>
-            </ion-buttons>
-          </ion-toolbar>
-          <ion-toolbar>
-            <ion-title>番組名を登録する</ion-title>
-          </ion-toolbar>
-        </IonHeader>
-        <IonItem>
+          </IonButton>
+            </IonButtons>
+          </IonToolbar>
           <IonInput
-            value={programName}
-            placeholder="番組名"
-            onIonChange={(e) => setProgramName(e.detail.value)}
+            value={labelName}
+            placeholder="ラベル名"
+            onIonChange={(e) => setLabelName(e.detail.value)}
 
           ></IonInput>
-
-        </IonItem>
+        </IonHeader>
         <IonItemSliding>
           <IonItem>
-            <ion-buttons slot="start">
-              <ion-button>
-                ＋
-          </ion-button>
-
-              <IonInput
-                value={programName}
-                placeholder="番組名"
-                onIonChange={(e) => setProgramName(e.detail.value)}
-              ></IonInput>
-            </ion-buttons>
+            <IonInput
+              value={programName}
+              placeholder="番組名"
+              onIonChange={(e) => setProgramName(e.detail.value)}
+            ></IonInput>
           </IonItem>
+          <IonButtons slot="start">
+            <IonButton>
+              ＋
+          </IonButton>
+          </IonButtons>
           <IonItemOptions side="end">
             <IonItemOption color="danger" onClick={() => console.log('share clicked')}>削除</IonItemOption>
           </IonItemOptions>
         </IonItemSliding>
-        <ion-button expand="block">ラベル印刷</ion-button>
+        <IonButton expand="block">ラベル印刷</IonButton>
       </IonContent>
     </IonPage >
   );
