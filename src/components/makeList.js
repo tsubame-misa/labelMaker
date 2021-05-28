59aeb06ff86aa313d0393df8a8647058d12129fc
Misato Watanabe <55613353+tsubame-misa@users.noreply.github.com>
2021-05-28 23:15:38 +0900

    0004リスト作成画面作成 (#5)
    * リスト画面の作成
    
    * Revert "リスト画面の作成"
    
    This reverts commit 107e8c4ee765f4f453885c813b2ffdddf24986b7.
    
    * リスト画面の作成
    
    * change Makelist
    
    * saaya uploadeed
    
    * 210521 saaya_last
    
    * saaya uploadeed
    
    Co-authored-by: Numakei <ghost.339.kei@gmail.com>
    Co-authored-by: sashimi390 <67630672+sashimi390@users.noreply.github.com>

--- src/components/makeList.js
+++ src/components/makeList.js
@@ -1,25 +1,74 @@
 import {
-    IonContent,
-    IonFab,
-    IonFabButton,
-    IonHeader,
-    IonIcon,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-  } from "@ionic/react";
-  import { addOutline } from "ionicons/icons";
-  import "../pages/Home.css";
-  
-  const makeList = () => {
-    return (
-      <IonPage>    
-        <IonContent fullscreen>
-          make List
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default makeList;
-  
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonItemSliding,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonAvatar,
+  IonBackButton,
+  IonButtons,
+  IonItemOption,
+  IonItemOptions,
+} from "@ionic/react";
+import { addOutline } from "ionicons/icons";
+import "../pages/Home.css";
+import { useEffect, useState } from "react";
+
+const MakeList = () => {
+  const [labelName, setLabelName] = useState();
+  const [programName, setProgramName] = useState();
+  const [data, setData] = useState([]);
+
+  return (
+    <IonPage>
+      <IonContent>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton defaultHref="/" />
+            </IonButtons>
+            <IonButtons slot="end">
+              <IonButton>
+                保存
+          </IonButton>
+            </IonButtons>
+          </IonToolbar>
+          <IonInput
+            value={labelName}
+            placeholder="ラベル名"
+            onIonChange={(e) => setLabelName(e.detail.value)}
+
+          ></IonInput>
+        </IonHeader>
+        <IonItemSliding>
+          <IonItem>
+            <IonInput
+              value={programName}
+              placeholder="番組名"
+              onIonChange={(e) => setProgramName(e.detail.value)}
+            ></IonInput>
+          </IonItem>
+          <IonButtons slot="start">
+            <IonButton>
+              ＋
+          </IonButton>
+          </IonButtons>
+          <IonItemOptions side="end">
+            <IonItemOption color="danger" onClick={() => console.log('share clicked')}>削除</IonItemOption>
+          </IonItemOptions>
+        </IonItemSliding>
+        <IonButton expand="block">ラベル印刷</IonButton>
+      </IonContent>
+    </IonPage >
+  );
+};
+
+export default MakeList;
